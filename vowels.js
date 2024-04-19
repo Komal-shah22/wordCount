9cd7bb3f6e6582ceca4ae3f18d42eae5ffe9b126
Komal-shah22 <160298356+Komal-shah22@users.noreply.github.com>
2024-04-19 12:07:19 +0100

    Add files via upload

---
+++ vowels.js
@@ -0,0 +1,17 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+const countvowels = async () => {
+    let input = await inquirer.prompt([{
+            name: "str",
+            message: "enter the text.....",
+            type: "input"
+        }]);
+    let count = 0;
+    for (let i of input.str) {
+        if (i === "a" || i === "e" || i === "i" || i === "o" || i === "u") {
+            count++;
+        }
+    }
+    console.log(`The number of vowels you used in this text is :  "${chalk.bold.greenBright(count)}"`);
+};
+export default countvowels;
