ddfce499fa848270741e64df6ef56a027a99aba6
Komal-shah22 <160298356+Komal-shah22@users.noreply.github.com>
2024-04-19 12:08:47 +0100

    Add files via upload

---
+++ mainfile.ts
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+import inquirer from "inquirer";
+import wordCounter from "./app.js";
+import result from "./index.js";
+import countvowels from "./vowels.js";
+let option = await inquirer.prompt([{
+    name:"click",
+    type:"list",
+    message:"Please one of the following...",
+    choices:["wordCounter","vowelsCount"]
+}]);
+if(option.click === "wordCounter"){
+    result(wordCounter)
+}else if(option.click === "vowelsCount"){
+   result (countvowels)
+}
