f9d347708fdfc37367b05fe93859fc45e690f01f
Komal-shah22 <160298356+Komal-shah22@users.noreply.github.com>
2024-04-19 12:05:45 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+import inquirer from "inquirer";
+async function result(fun) {
+    let condition = true;
+    do {
+        await fun();
+        let input = await inquirer.prompt([{
+                name: "answer",
+                type: "list",
+                message: "Do you want to countinue...",
+                choices: ["Yes", "No"]
+            }]);
+        if (input.answer === "No") {
+            condition = false;
+        }
+    } while (condition);
+}
+export default result;
